c3c2c12c3c967d7604c79b49a7fa475e25aee913
spences10 <spences10apps@gmail.com>
2021-02-16 18:12:08 +0000

    add zoom component for consisten appearance

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 /** @jsx jsx */
-import * as components from "@mdxp/components";
-import Deck from "@mdxp/core";
-import ReactDOM from "react-dom";
-import { jsx } from "theme-ui";
-import MDXPresentation from "./presentation.mdx";
+import * as components from '@mdxp/components'
+import Deck, { Zoom } from '@mdxp/core'
+import ReactDOM from 'react-dom'
+import { jsx } from 'theme-ui'
+import MDXPresentation from './presentation.mdx'
 
-ReactDOM.render(
-  <Deck components={components}>
-    <MDXPresentation />
-  </Deck>,
-  document.getElementById("root")
-);
+const Index = () => {
+  return (
+    <Zoom
+      maxWidth={1000}
+      width={1000}
+      aspectRatio={16 / 9}
+      sx={{ maxWidth: '100vw', maxHeight: '100vh' }}
+    >
+      <Deck components={components}>
+        <MDXPresentation />
+      </Deck>
+    </Zoom>
+  )
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'))
