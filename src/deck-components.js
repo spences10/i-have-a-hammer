bd139c422e927786ababab40b5d1f03777b523f7
spences10 <spences10apps@gmail.com>
2021-02-22 13:08:14 +0000

    add mdx embed tweet

--- src/deck-components.js
+++ src/deck-components.js
@@ -1,7 +1,9 @@
 import { Callout } from './components/callout'
 import CodeHighlight from './components/code'
+import { Tweet } from 'mdx-embed'
 
 export default {
   CodeHighlight,
   Callout,
+  Tweet,
 }
