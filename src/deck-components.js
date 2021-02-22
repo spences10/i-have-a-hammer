f7166cd11e07a01fd3d6afe7af39e3f9cd0867ce
spences10 <spences10apps@gmail.com>
2021-02-22 18:50:56 +0000

    add assets

--- src/deck-components.js
+++ src/deck-components.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Callout } from './components/callout'
 import CodeHighlight from './components/code'
-import { Tweet as MDXTweet } from 'mdx-embed'
+import { Tweet as MDXTweet, CodePen as MDXCodePen } from 'mdx-embed'
 
+// set the width on the tweet
 const Tweet = props => {
   return (
     <div style={{ width: '100%' }}>
       <MDXTweet {...props} />
+    </div>
+  )
+}
+// set the width
+const CodePen = props => {
+  return (
+    <div style={{ width: '70%' }}>
+      <MDXCodePen {...props} />
     </div>
   )
 }
@@ -15,4 +24,5 @@ export default {
   CodeHighlight,
   Callout,
   Tweet,
+  CodePen,
 }
